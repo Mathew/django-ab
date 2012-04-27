e0b86fd83837abb80ea61fad313f8d0b4e9c8216
Mathew Taylor <mat@mathewtaylor.co.uk>
2012-04-27 17:29:08 +0100

    add tox, fix 1.4 incompatibilities.

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -1,8 +1,20 @@
 from django.conf.urls.defaults import *
+try:
+    from django.views.generic.simple import direct_to_template
+    imported = True
+except:
+    from django.views.generic import TemplateView
+    imported = False
 
-
-urlpatterns = patterns('django.views.generic.simple',
-    (r'^test/$', 'direct_to_template', {'template': 'original.html'}),
-    (r'^other/$', 'direct_to_template', {'template': 'other.html'}),
-    (r'^goal/$', 'direct_to_template', {'template': 'goal.html'}),
-)
+if imported:
+    urlpatterns = patterns('',
+        (r'^test/$', direct_to_template, {'template': 'original.html'}),
+        (r'^other/$', direct_to_template, {'template': 'other.html'}),
+        (r'^goal/$', direct_to_template, {'template': 'goal.html'}),
+    )
+else:
+    urlpatterns = patterns('',
+        (r'^test/$', TemplateView.as_view(template_name='original.html')),
+        (r'^other/$', TemplateView.as_view(template_name='other.html')),
+        (r'^goal/$', TemplateView.as_view(template_name='goal.html')),
+    )
--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,5 @@
 import os
- 
+
 from django.conf import settings
 from django.test import TestCase
 
@@ -7,25 +7,31 @@ from ab.models import Test, Experiment
 
 
 class ABTests(TestCase):
-    urls = "ab.tests.test_urls"
+    urls = "tests.test_urls"
     fixtures = ["test_data"]
     template_dirs = [
         os.path.join(os.path.dirname(__file__), 'templates'),
     ]
-    
+
     def setUp(self):
         self.old_template_dir = settings.TEMPLATE_DIRS
-        settings.TEMPLATE_DIRS = self.template_dirs    
-        
+        settings.TEMPLATE_DIRS = self.template_dirs
+
     def tearDown(self):
         settings.TEMPLATE_DIRS = self.old_template_dir
-        
+
     def test_ab(self):
         # Careful here, response.template doesn't know where we are.
-    
+
         # It always looks like we loaded the original.
-        response = self.client.get("/test/")        
-        self.assertTrue(response.template.name, "original.html")
+        response = self.client.get("/test/")
+        print response
+        print dir(response)
+
+        try:
+            self.assertTrue(response.template.name, "original.html")
+        except AttributeError:
+            self.assertTrue(response.template_name, "original.html")
 
         # But we really just loaded one of the tests (template_name is the response content)
         test = Test.objects.get(template_name=response.content)
@@ -35,7 +41,7 @@ class ABTests(TestCase):
 
         # Tests are sticky.
         for _ in range(9):
-            responsex = self.client.get("/test/")        
+            responsex = self.client.get("/test/")
         self.assertEquals(response.content, responsex.content)
 
         # @@@ Do we need to keep querying to make sure this is up to date?
@@ -47,16 +53,20 @@ class ABTests(TestCase):
         # Going to another page doesn't effect anything.
         response = self.client.get("/other/")
         test = Test.objects.get(pk=test_id)
-        self.assertEquals(response.template.name, response.content)      
+
+        try:
+            self.assertEquals(response.template.name, response.content)
+        except AttributeError:
+            self.assertEquals(response.template_name, [response.content, ])
         self.assertEquals(test.hits, 1)
         self.assertEquals(test.conversions, 0)
-        
+
         # Going to the goal results in a conversion.
         response = self.client.get("/goal/")
-        test = Test.objects.get(pk=test_id)        
-        self.assertEquals(response.template.name, response.content)      
+        test = Test.objects.get(pk=test_id)
+        try:
+            self.assertEquals(response.template.name, response.content)
+        except AttributeError:
+            self.assertEquals(response.template_name, [response.content, ])
         self.assertEquals(test.hits, 1)
         self.assertEquals(test.conversions, 1)
-        
-        
-        
